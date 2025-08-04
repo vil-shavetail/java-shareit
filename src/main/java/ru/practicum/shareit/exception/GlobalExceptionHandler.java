3b71b1ed714d0ec97a7044c87eb867cb2c356f60
vil-shavetail <demid.ivan@gmail.com>
2025-08-04 22:03:43 +0300

    Sprint 14 - Some changes have been made to pass the Postman collection tests.

--- src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
@@ -1,47 +1,37 @@
 package ru.practicum.shareit.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final String ERROR_STRING = "error";
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<String> handleUserNotFound(NotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-    }
+    public Map<String, String> handleNotFoundException(NotFoundException e) {
+        return Map.of(ERROR_STRING, e.getMessage());    }
 
     @ExceptionHandler(DuplicateEmailException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseEntity<String> handleDuplicateEmail(DuplicateEmailException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    public Map<String, String> handleConflictException(DuplicateEmailException e) {
+        return Map.of(ERROR_STRING, e.getMessage());
     }
 
     @ExceptionHandler(ForbiddenException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<String> handleForbidden(ForbiddenException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    public Map<String, String> handleConflictException(ForbiddenException e) {
+        return Map.of(ERROR_STRING, e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(
-            MethodArgumentNotValidException e) {
-        Map<String, String> errors = new HashMap<>();
-        e.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    public Map<String, String> handleValidationException(final MethodArgumentNotValidException e) {
+        return Map.of(ERROR_STRING, e.getMessage());
     }
 }
