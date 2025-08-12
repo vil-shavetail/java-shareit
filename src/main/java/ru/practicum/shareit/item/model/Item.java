34b230983cb63cadb16b617e73f3e11d1c584978
vil-shavetail <demid.ivan@gmail.com>
2025-08-12 21:26:49 +0300

    Sprint 15 - Added the schema.sql script and JPA Entity for Item and User models.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,20 +1,31 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.*;
+import ru.practicum.shareit.user.User;
 
 /**
  * TODO Sprint add-controllers.
  */
-@Data
-@AllArgsConstructor
-@EqualsAndHashCode
-public class Item {
-    private Long id;
-    private String name;
-    private String description;
-    private Boolean available;
-    private Long ownerId;
-    private Long requestId;
-}
+    @Entity
+    @Table(name = "items")
+    @Getter @Setter @ToString
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    public class Item {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @Column(nullable = false)
+        private String name;
+        @Column(nullable = false)
+        private String description;
+        @Column(nullable = false)
+        private Boolean available;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "owner_id", nullable = false)
+        private User owner;
+        @Column(name = "request_id")
+        private Long requestId;
+    }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,17 +1,25 @@
 package ru.practicum.shareit.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
 
 /**
  * TODO Sprint add-controllers.
  */
-@Data
+@Entity
+@Table(name = "users")
+@Getter @Setter @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
+    @Email
     private String email;
 }
