70931ca397fa04926745f2fd46efec9737d81740
vil-shavetail <demid.ivan@gmail.com>
2025-08-13 13:36:12 +0300

    Sprint 15 - Fixed the emailExists method for UserRepository.

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -3,11 +3,10 @@ package ru.practicum.shareit.user.repository;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("SELECT COUNT(u) > 0 FROM User u WHERE LOWER(u.email) = LOWER(:email)")
     boolean emailExists(@NotBlank(message = "Email cannot be empty") @Email(message = "Invalid email format") String email);
-
-    boolean existsById(Long id);
 }
