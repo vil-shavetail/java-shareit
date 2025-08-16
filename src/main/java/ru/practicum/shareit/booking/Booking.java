9a1dca6807dcd2ee9fcaae10e937492de18664e9
vil-shavetail <demid.ivan@gmail.com>
2025-08-16 20:18:37 +0300

    Sprint 15 - Added booking functionality. First iteration: Added a booking model, BookingStatus enumeration, and a script to create a booking table in the database.

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -3,5 +3,40 @@ package ru.practicum.shareit.booking;
 /**
  * TODO Sprint add-bookings.
  */
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.lang.NonNull;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime start;
+    @NonNull
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime end;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booker_id", nullable = false)
+    private User booker;
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private BookingStatus status;
 }
---
+++ src/main/java/ru/practicum/shareit/booking/BookingStatus.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.booking;
+
+public enum BookingStatus {
+    ALL,
+    WAITING,
+    APPROVED,
+    REJECTED,
+    CANCELED;
+}
