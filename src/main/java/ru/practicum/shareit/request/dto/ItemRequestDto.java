2107fbb5deafe96b1a9fa19798c77a4d0d2e9d93
vil-shavetail <demid.ivan@gmail.com>
2025-10-03 14:59:07 +0300

    Sprint 16 - Modified itemRequestDto, ItemRequestServiceImpl. Added ItemRequestMapper.

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.request.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequestDto {
+    private Long id;
+    @NotBlank(message = "Description cannot be blank")
+    private String description;
+    private Long requesterId;
+    private LocalDateTime created;
 }
--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -1,11 +1,19 @@
 package ru.practicum.shareit.request.service;
 
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestMapper;
+import ru.practicum.shareit.request.repository.ItemRequestRepository;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.repository.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -13,9 +21,27 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ItemRequestServiceImpl implements ItemRequestService {
+    private final ItemRequestRepository itemRequestRepository;
+    private final UserRepository userRepository;
+
     @Override
+    @Transactional
     public ItemRequestDto createRequest(ItemRequestDto itemRequestDto, Long requesterId) {
-        return null;
+        log.info("Creating request for item, author: {}", requesterId);
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        if (itemRequestDto.getDescription() == null || itemRequestDto.getDescription().isEmpty()) {
+            throw new ValidationException("The description of the item request cannot be empty");
+        }
+        User requester = userRepository.findById(requesterId)
+                .orElseThrow(() -> new NotFoundException("User not found"));
+
+        ItemRequest itemRequest = ItemRequestMapper.toEntity(itemRequestDto, requester);
+        itemRequest.setCreated(currentDateTime);
+        ItemRequest savedItemRequest = itemRequestRepository.save(itemRequest);
+
+        log.info("Created item request: {}", savedItemRequest.getId());
+        return ItemRequestMapper.toDto(savedItemRequest);
     }
 
     @Override
@@ -30,6 +56,11 @@ public class ItemRequestServiceImpl implements ItemRequestService {
 
     @Override
     public List<ItemRequestDto> getAllRequests() {
-        return List.of();
+        log.info("Request for all itemRequests");
+        List<ItemRequestDto> itemRequests = itemRequestRepository.findAll().stream()
+                .map(ItemRequestMapper::toDto)
+                .toList();
+        log.info("ItemRequests found: {}", itemRequests.size());
+        return itemRequests;
     }
 }
