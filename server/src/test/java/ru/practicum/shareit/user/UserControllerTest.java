639ac3a62f77278ef8022c791476da3db9399315
vil-shavetail <demid.ivan@gmail.com>
2025-10-14 13:38:54 +0300

    Sprint 16 - Added test for the UserController.

--- server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -17,6 +17,8 @@ import org.springframework.test.web.servlet.ResultActions;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
 
+import java.util.List;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,7 +36,6 @@ class UserControllerTest {
 
     @BeforeEach
     void setUp() {
-        // Инициализация тестовых данных
         userDto = new UserDto();
         userDto.setId(1L);
         userDto.setName("Test User");
@@ -61,4 +62,62 @@ class UserControllerTest {
                 .andExpect(jsonPath("name").value("Test User"))
                 .andExpect(jsonPath("email").value("test@example.com"));
     }
+
+    @Test
+    void testGetUser() throws Exception {
+        when(userService.getUserById(1L))
+                .thenReturn(userDto);
+
+        ResultActions result = mockMvc.perform(
+                get("/users/1")
+        );
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("name").value("Test User"));
+    }
+
+    @Test
+    void testGetAllUsers() throws Exception {
+        List<UserDto> users = List.of(userDto);
+        when(userService.getAllUsers())
+                .thenReturn(users);
+
+        ResultActions result = mockMvc.perform(
+                get("/users")
+        );
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].name").value("Test User"));
+    }
+
+    @Test
+    void testUpdateUser() throws Exception {
+        when(userService.updateUser(1L, userDto))
+                .thenReturn(userDto);
+
+        ResultActions result = mockMvc.perform(
+                patch("/users/1")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(userDto))
+        );
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("name").value("Test User"))
+                .andExpect(jsonPath("email").value("test@example.com"));
+    }
+
+    @Test
+    void testDeleteUser() throws Exception {
+        doNothing().when(userService).deleteUser(1L);
+
+        ResultActions result = mockMvc.perform(
+                delete("/users/1")
+        );
+
+        result.andExpect(status().isNoContent());
+    }
+
 }
