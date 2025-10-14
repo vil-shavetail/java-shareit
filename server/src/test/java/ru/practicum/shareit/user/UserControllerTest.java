e130ae4fe4ca48838e99af14e98c4ec9d0cdcb99
vil-shavetail <demid.ivan@gmail.com>
2025-10-14 13:25:08 +0300

    Sprint 16 - Added test for controllers, first iteration.

---
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,65 @@
+package ru.practicum.shareit.user;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private ObjectMapper objectMapper;
+
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        // Инициализация тестовых данных
+        userDto = new UserDto();
+        userDto.setId(1L);
+        userDto.setName("Test User");
+        userDto.setEmail("test@example.com");
+
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    @Test
+    void testCreateUser() throws Exception {
+
+        when(userService.createUser(userDto))
+                .thenReturn(userDto);
+
+        ResultActions result = mockMvc.perform(
+                post("/users")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(userDto))
+        );
+
+        result.andExpect(status().isCreated())
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("name").value("Test User"))
+                .andExpect(jsonPath("email").value("test@example.com"));
+    }
+}
